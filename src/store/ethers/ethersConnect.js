6c1405d93c5025c43f3c7cb25498849b0160a37c
jzstern <jeremy.stern@connexta.com>
2020-10-22 21:02:25 -0600

    onward

--- src/store/ethers/ethersConnect.js
+++ src/store/ethers/ethersConnect.js
@@ -38,9 +38,9 @@ export const LOG_TRANSACTIONS = [
 ]
 
 // * max uint256 * 115792089237316195423570985008687907853269984665640564039457584007913129639935
-const CATALOG_CONTRACT_ADDRESS = '0x937c882Ed182CEf2A9174aC48e7a221474dcA1c5'
-const DAI_CONTRACT_ADDRESS = '0x13D282Daa4016396bc7294cAD4C855773253eb10'
-const artistPoolAbi = require('./abi/artistPool.json')
+const CATALOG_CONTRACT_ADDRESS = '0x1e0FcC8644b40A53f5272C8A0B5E5Bd9ca697553'
+const DAI_CONTRACT_ADDRESS = '0x65a0D7D0e47ccF0501985246542E5d88740B5E45'
+const artistPoolAbi = require('./abi/ArtistPool.json')
 const catalogAbi = require('./abi/catalog.json')
 const distributorAbi = require('./abi/IDistributor.json')
 const IERC20 = require('./abi/IERC20.json')
--- src/textile_constants/textile.js
+++ src/textile_constants/textile.js
@@ -3,4 +3,4 @@ export const KEY_INFO = {
     secret: 'bb35hh7bdano253ijv4rhi3sda7y57g5ep4atqqa'
 }
 
-export const THREAD_ID = 'bafk5hjqkc3mnevugyncfcjp6nuibllmolf3co7zsvkldbtozagjixaq'
+export const THREAD_ID = 'bafkwjubgxg3r2zohwerephljqis3p6jnoanpbtyxdck5iab4wk7bv4q'
